da2cd2549c04d5062a48e0e59507f44fcc3b2c53
Dorell James <galangdj@gmail.com>
2024-10-31 22:36:03 +0800

    chore: add resume link

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,16 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  redirects: async () => {
+    return [
+      {
+        source: '/resume',
+        destination:
+          'https://dorelljames.notion.site/Dorell-James-Resume-ed14bff98f164a4d8627a703902b7d8f',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default withMDX(nextConfig)
